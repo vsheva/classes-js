e673a04136e570be6cf72d6c80982e9b2a44aa2c
vsheva <valera.sheva@gmail.com>
2021-11-10 12:48:56 +0200

    3 задания решено

--- js/script.js
+++ js/script.js
@@ -1,30 +1,91 @@
 "use strict";
 
-class First {
-    constructor(name) {
-        this.name = name;
-    }
+//          --- 1 задание ----
 
-    hello() {
-        console.log(`Привет, ${this.name}, я метод родителя!`)
-    }
+// const calculator = {
+//
+//     sum: function(){
+//         let num1, num2, result
+//         num1 = document.getElementById("a").value;
+//         num1 = parseInt(num1)
+//         num2 = document.getElementById("b").value;
+//         num2 = parseInt(num2)
+//
+//         result = num1 + num2;
+//         console.log(result)
+//         document.getElementById("res").value = result;
+//     },
+//     mult: function(){
+//
+//         let num1, num2, result
+//         num1 = document.getElementById("a").value;
+//         num1 = parseInt(num1)
+//         num2 = document.getElementById("b").value;
+//         num2 = parseInt(num2)
+//
+//         result = num1 * num2;
+//         console.log(result)
+//
+//       document.getElementById("res").value = result;
+//     },
+//     show: function(){
+//          document.getElementById("sum").addEventListener("click", this.sum)
+//          document.getElementById("mult").addEventListener("click", this.mult)
+//     }
+// }
+//
+// calculator.show();
+
+
+
+
+
+
+// ---2 задание---
+
+//
+// function getResult(x,y){
+//     let result;
+//
+//     let z = Math.pow(x,y)
+//
+//     result = String(z).split('').reduce((a, b) => Number(a) + Number(b))
+//
+//
+//     return result
+// }
+//
+// console.log(getResult(4, 8))
+//
+//
+//
+
+//  --- 3 задание ---
+
+const cityArr = {
+    rus: ['Москва', 'Санк-Петербург', 'Новосибирск', 'Екатеринбург', 'Нижний Новгород', 'Казань', 'Челябинск'],
+    uk: ['Киев', 'Харьков', 'Одесса', 'Днепр', 'Донецк', 'Запорожье', 'Львов'],
+    bel: ['Минск', 'Гомель', 'Могилёв', 'Витебск', 'Гродно', 'Брест'],
+    jap: ['Токио', 'Киото', 'Осака', 'Иокогама']
 }
 
+let selectedCountry = document.querySelector("#country");
+let selectedCity = document.querySelector("#city");
 
-class Second extends First {
-    constructor(name, position) {
-        super(name)
-        this.position = position
+selectedCountry.addEventListener('change',function(){
+    console.log();
+    let cities =cityArr[this.value];
+    selectedCity.length = 0;
+
+
+
+    for(let i = 0; i < cities.length; i++) {
+
+      selectedCity.add(new Option(cities[i]));
+
     }
-
-    hello() {
-        super.hello()
-        console.log(`А я, ${this.position}, наследуемый метод!`)
-    }
-}
+})
 
 
-const test = new Second("Валера", "студент");
-test.hello();
 
 
